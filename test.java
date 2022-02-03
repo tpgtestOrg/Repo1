c0d457271320967d0f5a3075cdb96340c0bd8e5f
srivishal1 <vishal.srivastava@3pillarglobal.com>
2022-02-03 18:10:07 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.print("Java"); 
         readjdjfd
-            fdfdf
+            fdfdfksdfjsd
             updated 7.22
     }  
 }  
